a80b109ccb794cfe84b68de0065823c862b27a6e
Student408 <theloko.dev@gmail.com>
2025-06-12 21:39:47 +0530

    Refactor build.gradle.kts: reorganize kotlinOptions and defaultConfig for improved readability

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,11 +13,11 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
-    }    defaultConfig {
+    defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.myapp"
         // You can update the following values to match your application needs.
