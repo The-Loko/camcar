16e4e3db8a102047bffd9efdf6f6ac0f24889d58
Student408 <theloko.dev@gmail.com>
2025-06-12 22:29:21 +0530

    Fix formatting in build.gradle.kts: separate compileOptions and kotlinOptions for improved clarity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -13,7 +13,9 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
-    }    kotlinOptions {
+    }
+
+    kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
 
